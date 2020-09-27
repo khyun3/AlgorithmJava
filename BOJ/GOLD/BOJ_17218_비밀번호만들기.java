1b85fe2494e2002f1179ec06c82bebe5c1e4013c
KiHyeonYun <ykh094@naver.com>
2020-09-27 12:20:19 +0900

    2020.09.27

---
+++ BOJ/GOLD/BOJ_17218_비밀번호만들기.java
@@ -0,0 +1,17 @@
+package GOLD;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_17218_비밀번호만들기 {
+
+	static char[] pw1, pw2;
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		pw1 = br.readLine().toCharArray();
+		pw2 = br.readLine().toCharArray();
+
+		
+	}
+}
