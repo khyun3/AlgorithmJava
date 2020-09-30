04e612d6885eb61d51f8c624108c9d20a6d7a046
KiHyeonYun <ykh094@naver.com>
2020-10-01 04:02:21 +0900

    2020.10.01

--- BOJ/SILVER/BOJ_1181_단어정렬.java
+++ BOJ/SILVER/BOJ_1181_단어정렬.java
@@ -4,24 +4,35 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.TreeSet;
 
-
 public class BOJ_1181_단어정렬 {
-	
+	static class Word implements Comparable<Word>{
+		String w;
+		
+		public Word(String w) {
+			this.w = w;
+		}
+		@Override
+		public int compareTo(Word o) {
+			int res = Integer.compare(this.w.length(), o.w.length());
+			if(res==0) {
+				return this.w.compareTo(o.w);
+			}
+			return res;
+		}
+	}
 	static int N;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
-		TreeSet<String> ts = new TreeSet<>();
-	
+		TreeSet<Word> list = new TreeSet<>();
+		
 		for (int i = 0; i < N; i++) {
-			String str = br.readLine().toString();
-			ts.add(str);
-			
+			list.add(new Word(br.readLine().trim()));
 		}
-		//Collections.sort();
-		for (String s : ts) {
-			System.out.println(s);
+		for (Word word : list) {
+			sb.append(word.w).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
