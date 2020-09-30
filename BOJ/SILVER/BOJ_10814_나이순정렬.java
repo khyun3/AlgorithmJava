da364957ca38682ddabecb163156e8f72db1e053
KiHyeonYun <ykh094@naver.com>
2020-09-30 23:57:12 +0900

    2020.09.30

---
+++ BOJ/SILVER/BOJ_10814_나이순정렬.java
@@ -0,0 +1,37 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_10814_나이순정렬 {
+	static class Member {
+		int age;
+		String name;
+		public Member(int age, String name) {
+			this.age = age;
+			this.name = name;
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		List<Member> mList = new LinkedList<>();
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			mList.add(new Member(Integer.parseInt(st.nextToken()),st.nextToken()));
+		}
+		Collections.sort(mList, (Member m1, Member m2)-> m1.age-m2.age);
+		StringBuilder sb = new StringBuilder();
+		for (Member m : mList) {
+			sb.append(m.age).append(" ").append(m.name).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
---
+++ BOJ/SILVER/BOJ_2164_카드2.java
@@ -0,0 +1,23 @@
+package SILVER;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2164_카드2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		Queue<Integer> que = new LinkedList<Integer>();
+		for (int i = 1; i <= N; i++) {
+			que.offer(i);
+		}
+		while(--N!=0) {
+			que.poll(); //맨위 버리기
+			que.offer(que.poll()); //맨위 뒤로
+		}
+		System.out.println(que.poll());
+		sc.close();
+	}
+}
