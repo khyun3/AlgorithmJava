4b81dbfb0c55a1c31c3b8846fbc26d546da7ef72
KiHyeonYun <ykh094@naver.com>
2020-10-06 00:15:20 +0900

    2020.10.06 String

---
+++ BOJ/SILVER/BOJ_1764_듣보잡.java
@@ -0,0 +1,38 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_1764_듣보잡 {
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken()); //듣도 못한 사람
+		int M = Integer.parseInt(st.nextToken()); //보도 못한 사람
+		int cnt = 0;
+		
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < N; i++) {
+			map.put(br.readLine().trim(), 1);
+		}
+		List<String> list = new ArrayList<>();
+		
+		for (int i = 0; i < M; i++) {
+			String str = br.readLine().trim();
+			if(map.containsKey(str)){
+				list.add(str);
+				cnt++;
+			}
+		}
+		Collections.sort(list);
+		for (String s : list) { sb.append(s).append("\n");}
+		System.out.println(cnt+"\n"+sb);
+	}
+}
