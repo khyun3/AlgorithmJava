53a2d79af0c67fefac54028d74d2f831d57d795f
KiHyeonYun <ykh094@naver.com>
2020-10-07 15:04:46 +0900

    2020.10.07 DFS, greedy

---
+++ BOJ/SILVER/BOJ_1931_회의실배정.java
@@ -0,0 +1,44 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1931_회의실배정 {
+	static class Conf implements Comparable<Conf>{
+		int s; int e;
+		public Conf(int s, int e) {this.s = s; this.e = e;}
+
+		@Override
+		public int compareTo(Conf o) {
+			if(this.e==o.e) {
+				return Integer.compare(this.s, o.s);
+			}
+			return Integer.compare(this.e, o.e);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		Conf [] cArr = new Conf[N]; 
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			cArr[i] = new Conf(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+		}
+		Arrays.sort(cArr);
+		
+		int cnt = 0;
+		int end = 0;
+		for (int i = 0; i < N; i++) {
+			if(end<=cArr[i].s) {
+				end = cArr[i].e;
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
