ea87c22fd6042d519e7d04697f458bae37b4e868
KiHyeonYun <ykh094@naver.com>
2022-08-15 20:37:55 +0900

    2022.08.15 다이나믹프로그래밍 - RE

--- BOJ/SILVER/BOJ_1463_1로만들기.java
+++ BOJ/SILVER/BOJ_1463_1로만들기.java
@@ -1,6 +1,9 @@
 package SILVER;
 
+import java.sql.DriverManager;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.IntConsumer;
 
 public class BOJ_1463_1로만들기 {
 	
@@ -9,21 +12,23 @@ public class BOJ_1463_1로만들기 {
 		
 		int N = sc.nextInt();
 		int [] D = new int[N+1]; 
-		
-		D[0] = 0;
+
 		D[1] = 0; //1일 때는 연산 x
 
 		for (int i = 2; i <= N; i++) {
-		int A = Integer.MAX_VALUE;
-		int B = Integer.MAX_VALUE;
-		
-		if(i%3==0) B = D[i/3]+1;		//  /3
-		else if(i%2==0) B = D[i/2]+1;	//  /2
-		A = D[i-1]+1;					//  -1
-			
-		D[i] = Math.min(A, B);
+			int A = Integer.MAX_VALUE;
+			int B = Integer.MAX_VALUE;
+			int C = Integer.MAX_VALUE;
+			if(i%3==0) {
+				A = D[i/3]+1;	//  /3
+			}
+			if(i%2==0) {
+				B = D[i/2]+1;	//  /2
+			}
+			C = D[i-1]+1;		//  -1
+
+			D[i] = Math.min(Math.min(A, B), C);
 		}
-		
 		System.out.println(D[N]);
 	}
 }
