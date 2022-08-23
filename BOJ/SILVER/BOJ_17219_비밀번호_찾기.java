0c94b99603a1d96bdfc129e883cbeb979edcb9a6
KiHyeonYun <ykh094@naver.com>
2022-08-23 21:56:47 +0900

    자료구조-Map

---
+++ BOJ/SILVER/BOJ_17219_비밀번호_찾기.java
@@ -0,0 +1,29 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BOJ_17219_비밀번호_찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());//사이트 비밀번호
+        int M = Integer.parseInt(st.nextToken());//찾을 사이트
+
+        HashMap<String, String> pwdMap = new HashMap<>();
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            pwdMap.put(st.nextToken(), st.nextToken());
+        }
+
+        while(M -- > 0) {
+            sb.append(pwdMap.get(br.readLine())).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
