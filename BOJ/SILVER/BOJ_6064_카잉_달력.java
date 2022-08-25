b90e2831a750e1ffd12dc13bdd2efa6613718191
KiHyeonYun <ykh094@naver.com>
2022-08-25 22:47:39 +0900

    수학-정수론,중국인의 나머지 정리

--- BOJ/SILVER/BOJ_6064_카잉_달력.java
+++ BOJ/SILVER/BOJ_6064_카잉_달력.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 //TODO : 다시 풀어보기
 //중국인의 나머지 정리
-//
 public class BOJ_6064_카잉_달력 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
