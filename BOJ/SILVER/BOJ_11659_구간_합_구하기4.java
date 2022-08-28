a230a4c00a21d7518d8c730b2f10ce4fa05b65bd
KiHyeonYun <ykh094@naver.com>
2022-08-28 13:40:52 +0900

    수학-누적 합

---
+++ BOJ/SILVER/BOJ_11659_구간_합_구하기4.java
@@ -0,0 +1,42 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11659_구간_합_구하기4 {
+    static int [] sum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        sum = new int [N+1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        init(N, st);
+
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(addSection(start, end)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static void init(int n, StringTokenizer st) {
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static int addSection(int start, int end) {
+        return sum[end] - sum[start-1];
+    }
+}
