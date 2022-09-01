c804493f72b455ed3789177e5debe645af7cdb89
KiHyeonYun <ykh094@naver.com>
2022-09-01 22:41:28 +0900

    구현

---
+++ BOJ/BRONZE/BOJ_5337_웰컴.java
@@ -0,0 +1,11 @@
+package BRONZE;
+
+public class BOJ_5337_웰컴 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n")
+                .append("|  | _ | _. _ ._ _  _\n")
+                .append("|/\\|(/.|(_.(_)[ | )(/.\n");
+        System.out.print(sb);
+    }
+}
