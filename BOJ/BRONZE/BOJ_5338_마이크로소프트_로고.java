79c4c60b3e90c98e5dfbcd639880501e258719c0
KiHyeonYun <ykh094@naver.com>
2022-09-02 20:25:03 +0900

    구현

---
+++ BOJ/BRONZE/BOJ_5338_마이크로소프트_로고.java
@@ -0,0 +1,13 @@
+package BRONZE;
+
+public class BOJ_5338_마이크로소프트_로고 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n")
+          .append("'-..-'|   ||   |\n")
+          .append("'-..-'|_.-;;-._|\n")
+          .append("'-..-'|   ||   |\n")
+          .append("'-..-'|_.-''-._|\n");
+        System.out.print(sb);
+    }
+}
