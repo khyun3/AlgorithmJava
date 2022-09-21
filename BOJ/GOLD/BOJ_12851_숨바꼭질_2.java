c2e7798441711d575e8f7c42e5c091dac56a6a14
윤기현 <khyun@egap.co.kr>
2022-09-21 15:41:02 +0900

    그래프-BFS

--- BOJ/GOLD/BOJ_12851_숨바꼭질_2.java
+++ BOJ/GOLD/BOJ_12851_숨바꼭질_2.java
@@ -5,68 +5,60 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-//방문체크 다시할 것 (틀렸음)
+
 public class BOJ_12851_숨바꼭질_2 {
-    static int[] result = new int[] {Integer.MAX_VALUE, 0};
+    static int minMvCnt = 1000000, dupCnt;
     static boolean[] visited = new boolean[100001];
 
     static class Seek {
         int x;
-        int moveCnt;
+        int mvCnt;
 
-        public Seek(int x, int moveCnt) {
+        public Seek(int x, int mvCnt) {
             this.x = x;
-            this.moveCnt = moveCnt;
+            this.mvCnt = mvCnt;
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
         bfs(n, k);
-        System.out.println(result[0] + "\n" + result[1]);
+        System.out.println(minMvCnt + "\n" + dupCnt);
     }
 
     static void bfs(int n, int k) {
         Queue<Seek> que = new LinkedList<>();
         que.offer(new Seek(n, 0));
-        visited[n] = true;
 
         while (!que.isEmpty()) {
-            Seek curr = que.poll();
-            visited[k] = false;
-            if (curr.x == k) {
-                if (curr.moveCnt < result[0]) {
-                    result[0] = curr.moveCnt;
-                    result[1] = 1;
-                    break;
-                }
-            }
+            Seek cur = que.poll();
+            visited[cur.x] = true;
 
-            int a = curr.x - 1;
+            //최초 방문보다 이동 횟수가 많다면 종료
+            if(minMvCnt < cur.mvCnt){
+                break;
+            }
+            //동생과 만났다면
+            if(cur.x == k){
+                minMvCnt = cur.mvCnt;
+                dupCnt++;
+                continue;
+            }
+            //이동 방법 3가지
+            int a = cur.x - 1;
+            int b = cur.x + 1;
+            int c = cur.x * 2;
             if (a >= 0 && !visited[a]) {
-                que.offer(new Seek(a, curr.moveCnt + 1));
+                que.offer(new Seek(a, cur.mvCnt + 1));
             }
-            int b = curr.x + 1;
             if (b <= 100000 && !visited[b]) {
-                que.offer(new Seek(b, curr.moveCnt + 1));
+                que.offer(new Seek(b, cur.mvCnt + 1));
             }
-            int c = curr.x * 2;
-            if (c > 0 && c <= 100000 && !visited[c]) {
-                que.offer(new Seek(c, curr.moveCnt + 1));
-            }
-            if(a >= 0) {visited[a] = true;}
-            if(b <= 100000) {visited[b] = true;}
-            if(c <= 100000) {visited[c] = true;}
-        }
-        while(!que.isEmpty()) {
-            Seek next = que.poll();
-            if (next.x == k && next.moveCnt == result[0]) {
-                    result[1]++;
+            if (c <= 100000 && !visited[c]) {
+                que.offer(new Seek(c, cur.mvCnt + 1));
             }
         }
     }
