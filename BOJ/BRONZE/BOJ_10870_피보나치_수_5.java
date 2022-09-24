988bb958d5d191b7fdccfac54bbf217efeb61b58
KiHyeonYun <ykh094@naver.com>
2022-09-24 21:25:17 +0900

    수학-구현

---
+++ BOJ/BRONZE/BOJ_10870_피보나치_수_5.java
@@ -0,0 +1,16 @@
+package BRONZE;
+
+import java.util.Scanner;
+
+public class BOJ_10870_피보나치_수_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] fibo = new int [21];
+        fibo[1] = 1;
+        for (int i = 2; i <= 20; i++) {
+            fibo[i] = fibo[i-2] + fibo[i-1];
+        }
+
+        System.out.print(fibo[sc.nextInt()]);
+    }
+}
