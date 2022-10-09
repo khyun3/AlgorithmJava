c428fa7a3813575380fd80f1dae9c692a49e908b
KiHyeonYun <ykh094@naver.com>
2022-10-09 22:09:05 +0900

    구현

---
+++ BOJ/BRONZE/BOJ_와이버스_부릉부릉.java
@@ -0,0 +1,17 @@
+package BRONZE;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_와이버스_부릉부릉 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int bs =  Integer.parseInt(st.nextToken());
+        while (bs-- > 0) {
+            br.readLine();
+        }
+        System.out.println("비와이");
+    }
+}
