86f57e6afe88811a13f2ef68d3c7d070b4903c54
윤기현 <uplift@inbody.com>
2023-10-12 08:29:02 +0900

    문자열,정규표현식

---
+++ BOJ/GOLD/BOJ_1013_Contact.java
@@ -0,0 +1,18 @@
+package GOLD;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1013_Contact {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            boolean yes = br.readLine().matches("(100+1+|01)+");
+            answer.append(yes ? "YES\n": "NO\n");
+        }
+        System.out.print(answer);
+    }
+}
