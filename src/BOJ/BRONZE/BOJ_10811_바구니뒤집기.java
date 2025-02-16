c84efc104030a9daa0a7c7b77406c678537db875
KiHyeonYun <ykh094@naver.com>
2025-02-16 20:06:39 +0900

    구현, 시뮬레이션

---
+++ src/BOJ/BRONZE/BOJ_10811_바구니뒤집기.java
@@ -0,0 +1,47 @@
+package BOJ.BRONZE;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10811_바구니뒤집기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] token = br.readLine().split(" ");
+        int n = Integer.parseInt(token[0]);
+        int m = Integer.parseInt(token[1]);
+        int[] baskets = makeBaskets(n);
+        for (int i = 0; i < m; i++) {
+            token = br.readLine().split(" ");
+            reverse(baskets, Integer.parseInt(token[0]) - 1, Integer.parseInt(token[1]) - 1);
+        }
+        print(baskets);
+    }
+
+    static int[] makeBaskets(int n) {
+        int[] baskets = new int[n];
+        for (int i = 0; i < n; i++) {
+            baskets[i] = i + 1;
+        }
+        return baskets;
+    }
+
+    static void reverse(int[] baskets, int i, int j) {
+        //i ~ j 까지 역순으로 돌린다.
+        int cnt = (j - i + 1) / 2;
+        for (int k = 0; k < cnt; k++) {
+            int temp = baskets[i + k];
+            baskets[i + k] = baskets[j - k];
+            baskets[j - k] = temp;
+        }
+    }
+
+    static void print(int[] baskets) {
+        StringBuilder sb = new StringBuilder();
+        for (int b : baskets) {
+            sb.append(b).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
