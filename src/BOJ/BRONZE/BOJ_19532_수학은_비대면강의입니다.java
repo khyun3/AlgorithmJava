14395fd9f3de963085720374eb4e389c8d61df25
KiHyeonYun <ykh094@naver.com>
2025-02-21 21:15:35 +0900

    수학, 브루트포스

---
+++ src/BOJ/BRONZE/BOJ_19532_수학은_비대면강의입니다.java
@@ -0,0 +1,33 @@
+package BOJ.BRONZE;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_19532_수학은_비대면강의입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a, b, c, d, e, f;
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        f = Integer.parseInt(st.nextToken());
+
+        System.out.print(solution(a, b, c, d, e, f));
+    }
+
+    static String solution(int a, int b, int c, int d, int e, int f) {
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    return x + " " + y;
+                }
+            }
+        }
+        return "";
+    }
+}
