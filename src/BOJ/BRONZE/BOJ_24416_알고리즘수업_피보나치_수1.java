c2bcbb68b40ceeac1b04e445df7cb752c821d0e8
kihyeon.Yun <ykh094@naver.com>
2025-03-18 18:29:27 +0900

    Create BOJ_24416_알고리즘수업_피보나치_수1.java
    수학, 다이나믹 프로그래밍

---
+++ src/BOJ/BRONZE/BOJ_24416_알고리즘수업_피보나치_수1.java
@@ -0,0 +1,40 @@
+package BOJ.BRONZE;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_24416_알고리즘수업_피보나치_수1 {
+	 static int count1 = 0, count2 = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        fib(N);
+        fibonacci(N);
+        System.out.println(count1 + " " + count2);
+    }
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            count1++;
+            return 1;
+        } else
+            return fib(n - 1) + fib(n - 2);
+    }
+
+    static int fibonacci(int N) {
+        int[] f = new int[N + 1];
+        f[1] = 1;
+        f[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+            count2++;
+        }
+
+        return f[N];
+
+    }
+}
