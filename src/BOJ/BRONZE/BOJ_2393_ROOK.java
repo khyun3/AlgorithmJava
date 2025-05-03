d9907e85432b87123bb71084837338f8eae5ea45
kihyeon.yun <ykh094@naver.com>
2025-05-03 23:20:25 +0900

    문자열

---
+++ src/BOJ/BRONZE/BOJ_2393_ROOK.java
@@ -0,0 +1,21 @@
+package BOJ.BRONZE;
+
+public class BOJ_2393_ROOK {
+    public static void main(String[] args) {
+        String rook = "  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\";
+
+        System.out.println(rook);
+    }
+}
